448b38af070b599bd091d44e84e18e331717d52b
ASSASSINYM <darlingiloveu@sina.com>
2020-12-16 11:39:47 +0800

    multiple targets demo

--- TargetsDemo/ViewController.m
+++ TargetsDemo/ViewController.m
@@ -7,6 +7,14 @@
 
 #import "ViewController.h"
 
+//#if DEVELOPMENT
+//static NSString *baseURL = @"DEV URL"
+//#else
+//static NSString *baseURL = @"REL URL"
+//#endif
+
+
+
 @interface ViewController ()
 
 @end
@@ -16,6 +24,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+#if DEVELOPMENT
+    NSString *baseURL = @"DEV URL";
+#else
+    NSString *baseURL = @"REL URL";
+#endif
+    
+    NSLog(@"%@",baseURL);
+    
+}
+
+
+-(void)doSomethings {
+//#if DEVELOPMENT
+}
+
+-(void)develop {
+    
 }
 
 
